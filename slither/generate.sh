be08ebbabb2b095e0307e9db3108a118aeb764da
exhyn <exhyn@protonmail.com>
2021-05-01 09:26:21 -0300

    self audit for Yogi

--- slither/generate.sh
+++ slither/generate.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 set -ex
 
+# Run cmd and log
+run () {
+  printf "$ $1\n\n" &>> $2
+  $1 &>> $2
+  printf "\n\n" &>> $2
+}
+
+# Cleanup
+pushd slither
+([ -d tmp ] && rm -f tmp/*) || mkdir tmp
+[ -d reports ] || mkdir reports
+
+# Yogi.sol
+solc-select use 0.8.3
+run "slither contracts/Yogi.sol --print human-summary" tmp/Yogi.sol.ansi
+run "slither-check-erc contracts/Yogi.sol Yogi" tmp/Yogi.sol.ansi
+run "slither contracts/Yogi.sol --print contract-summary" tmp/Yogi.sol.ansi
+cat tmp/Yogi.sol.ansi | ansi2html > reports/Yogi.sol.html
+
+popd
--- slither/install.sh
+++ slither/install.sh
@@ -3,4 +3,5 @@ set -ex
 
 pip3 install slither-analyzer
 pip3 install solc-select
+pip3 install ansi2html
 solc-select install 0.8.3 0.6.12 0.5.17
