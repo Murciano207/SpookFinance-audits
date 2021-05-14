0a1f0f1ca6d90ea6f7d880e956e9dd13c1dd3f58
jadavasudev <jadavasudev@protonmail.com>
2021-05-14 01:28:00 -0300

    Removed duplicated license files from Yogi.sol.

--- slither/contracts/Yogi.sol
+++ slither/contracts/Yogi.sol
@@ -83,8 +83,6 @@ interface IERC20 {
 
 // File @openzeppelin/contracts/utils/Context.sol@v4.0.0
 
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 /*
@@ -110,8 +108,6 @@ abstract contract Context {
 
 
 // File @openzeppelin/contracts/token/ERC20/ERC20.sol@v4.0.0
-
-// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
@@ -415,8 +411,6 @@ contract ERC20 is Context, IERC20 {
 
 
 // File contracts/Yogi.sol
-
-// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
